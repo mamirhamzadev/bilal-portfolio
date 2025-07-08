b47b6b6b8a96b940877f2c47eca3bed93ff23a57
Muhammad Amir Hamza <mamirhamza.dev@gmail.com>
2025-07-08 12:57:25 +0500

    fixed an issue with callback

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,13 +2,7 @@
 	"use strict";
 
 	// Parallax scroll effect js
-	document.querySelectorAll(".move-with-cursor").forEach(a => {
-		document.addEventListener("mousemove", function (e) {
-			var t = e.clientX,
-				e = e.clientY;
-			a.style.transition = "transform 0.6s cubic-bezier(0.34, 1.56, 0.64, 1)", a.style.transform = `translate(${.01 * t}px, ${.01 * e}px) rotate(${.01 * (t + e)}deg)`
-		})
-	}),
+	
 
 		// Magnific popup js
 		$(".parent-container").magnificPopup({
@@ -18,25 +12,5 @@
 				enabled: true,
 			},
 		});
-
-	// Article publications slider js
-	$(".article-publications-slider").slick({
-		slidesToShow: 2,
-		slidesToScroll: 1,
-		autoplay: false,
-		dots: false,
-		infinite: true,
-		arrows: true,
-		speed: 500,
-		prevArrow: '<i class="fas left icon fa-arrow-left"></i>',
-		nextArrow: '<i class="fas right icon fa-arrow-right"></i>',
-		responsive: [{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1,
-			}
-		},]
-	});
 
 })(jQuery);
--- src/pages/templates/AppBackground.jsx
+++ src/pages/templates/AppBackground.jsx
@@ -2,21 +2,26 @@ import { Outlet } from "react-router-dom"
 import { BG_3D_1_IMAGE, BG_3D_2_IMAGE, BG_IMAGE } from "../../contants/images"
 
 function AppBackground() {
+    const handleMovement = (event) => {
+        var t = event.clientX,
+            e = event.clientY;
+        event.currentTarget.style.transition = "transform 0.6s cubic-bezier(0.34, 1.56, 0.64, 1)";
+        event.currentTarget.style.transform = `translate(${.01 * t}px, ${.01 * e}px) rotate(${.01 * (t + e)}deg)`;
+    }
+
     return (
         <>
-            <div className="background-shapes">
-                <div className="shape-1 common-shape">
-                    <img src={BG_IMAGE} alt="banner-shape-1" />
-                </div>
-                <div className="shape-2 common-shape">
-                    <img src={BG_IMAGE} alt="banner-shape-1" />
-                </div>
-                <div className="threed-shape-1 move-with-cursor" data-value={1}>
-                    <img src={BG_3D_1_IMAGE} alt="object-3d-1" />
-                </div>
-                <div className="threed-shape-2 move-with-cursor" data-value={1}>
-                    <img src={BG_3D_2_IMAGE} alt="object-3d-2" />
-                </div>
+            <div className="shape-1 common-shape">
+                <img src={BG_IMAGE} alt="banner-shape-1" />
+            </div>
+            <div className="shape-2 common-shape">
+                <img src={BG_IMAGE} alt="banner-shape-1" />
+            </div>
+            <div onMouseMove={handleMovement} className="threed-shape-1 move-with-cursor" data-value={1}>
+                <img src={BG_3D_1_IMAGE} alt="object-3d-1" />
+            </div>
+            <div onMouseMove={handleMovement} className="threed-shape-2 move-with-cursor" data-value={1}>
+                <img src={BG_3D_2_IMAGE} alt="object-3d-2" />
             </div>
 
             <Outlet />
